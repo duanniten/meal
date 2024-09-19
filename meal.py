929304de7e4fc2e48aa0035507227d4ff52b6e7b
duanniten <duann_@hotmail.com>
2024-09-18 22:42:44 -0300

    inplemente convert

--- meal.py
+++ meal.py
@@ -1,10 +1,13 @@
 def main():
-    ...
+    time = input("What time is it? ").strip()
+    
 
 
-def convert(time):
-    ...
-
+def convert(time:str):
+    timeParts = time.split(sep=":")
+    hour  = float(timeParts[0])
+    minutes = float(timeParts[1]) / 60
+    return hour + minutes
 
 if __name__ == "__main__":
     main()
