5c0fd841cd62fb07189b193553cd50d34e5a85cd
duanniten <duann_@hotmail.com>
2024-09-18 22:49:01 -0300

    implemetation main

--- meal.py
+++ meal.py
@@ -1,7 +1,15 @@
 def main():
     time = input("What time is it? ").strip()
-    
+    time = convert(time)
 
+    if 7 <= time <= 8:
+        print("breakfast time")
+    elif 12 <= time <=13:
+        print("lunch time")
+    elif 18 <= time <= 19:
+        print("dinne time")
+    
+        
 
 def convert(time:str):
     timeParts = time.split(sep=":")
